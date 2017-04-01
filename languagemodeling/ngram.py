3888dfd2c455465f3a9ddd01a0659b9624a3ba3e
Robertino Nicolazzi <robertnn@outlook.com>
2017-04-01 13:45:06 -0300

    Extension clase NGram. Se agrego el calculo de probabilidades condicionales de tokens, la probabilidad de una sentencia, probabilidad de una sentencia usando prob logaritmicas y el conteo para calcular las msimas

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -1,7 +1,9 @@
 # https://docs.python.org/3/library/collections.html
 from collections import defaultdict
+from math import log 
 
-
+INICIO = '<s>'
+FINAL = '</s>'
 class NGram(object):
 
     def __init__(self, n, sents):
@@ -14,10 +16,79 @@ class NGram(object):
         self.counts = counts = defaultdict(int)
 
         for sent in sents:
+            sent = [INICIO for i in range(n-1)] + sent + [FINAL]
             for i in range(len(sent) - n + 1):
                 ngram = tuple(sent[i: i + n])
                 counts[ngram] += 1
                 counts[ngram[:-1]] += 1
+
+    def cond_prob(self, token, prev_tokens=None):
+        """Conditional probability of a token.
+ 
+        token -- the token.
+        prev_tokens -- the previous n-1 tokens (optional only if n = 1).
+        """
+        if not prev_tokens:
+            prev_tokens = []
+
+        assert len(prev_tokens) == self.n - 1
+
+        tokens = prev_tokens + [token]
+        a = self.counts[tuple(tokens)]
+        b = self.counts[tuple(prev_tokens)]
+
+        res = 0
+        if b != 0:
+            res = a / float(b)
+
+        return res
+
+
+    def sent_prob(self, sent):
+        """Probability of a sentence. Warning: subject to underflow problems.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        n = self.n
+        ant = [INICIO for i in range(n-1)]
+        sent = ant + sent + [FINAL]
+        result = 1
+        for i in range(n-1,len(sent)):  #Rango inicia en n-1 para tomar la primer palabra distinta de <s>
+            word = sent[i]
+            prev_tokens = sent[i-n+1:i]
+            result *= self.cond_prob(word, prev_tokens)
+
+        return result 
+
+ 
+    def sent_log_prob(self, sent):
+        """Log-probability of a sentence.
+ 
+        sent -- the sentence as a list of tokens.
+        """
+        n = self.n
+        ant = [INICIO for i in range(n-1)]
+        sent = ant + sent + [FINAL]
+        result = 0
+        for i in range(n-1,len(sent)):  #Rango inicia en n-1 para tomar la primer palabra distinta de <s>
+            word = sent[i]
+            prev_tokens = sent[i-n+1:i]
+            temp = self.cond_prob(word, prev_tokens)
+
+            if temp == 0:
+                result += float('-inf')
+            else:
+                result += log(temp,2)
+
+        return result 
+
+    def count(self, tokens):
+        """Count for an n-gram or (n-1)-gram.
+ 
+        tokens -- the n-gram or (n-1)-gram tuple.
+        """
+
+        return self.counts[tokens]
 
     def prob(self, token, prev_tokens=None):
         n = self.n
