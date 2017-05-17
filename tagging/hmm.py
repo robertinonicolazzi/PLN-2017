5374552910ba76a54fc59c5f52e539683e58f515
Robertino Nicolazzi <robertnn@outlook.com>
2017-05-17 15:56:05 -0300

    pasa los test de hmm, falta revisar errores viterbi

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from math import log2
+
+
 class HMM:
  
     def __init__(self, n, tagset, trans, out):
@@ -7,10 +11,14 @@ class HMM:
         trans -- transition probabilities dictionary.
         out -- output probabilities dictionary.
         """
-        self.tag_set = tagset
+        self.tag_set = set(tagset)
         self.n = n
+
+        #funcion q
         self.trans = trans
-        self.out = out
+
+        #funcion e
+        self.out = dict(out)
  
     def tagset(self):
         """Returns the set of tags.
@@ -23,7 +31,7 @@ class HMM:
         tag -- the tag.
         prev_tags -- tuple with the previous n-1 tags (optional only if n = 1).
         """
-        return self.trans.get(prev_tags).get(word,0)
+        return self.trans.get(tuple(prev_tags),{}).get(tag,0.0)
  
     def out_prob(self, word, tag):
         """Probability of a word given a tag.
@@ -39,7 +47,19 @@ class HMM:
         Warning: subject to underflow problems.
  
         y -- tagging.
+
         """
+        #Generar el producto de los q
+        prob = 1.0
+        temp_y = ['<s>']*(self.n-1)+ list(y) + ['</s>']
+        n = self.n
+        for i in range(n-1,len(temp_y)):
+            tag = temp_y[i]
+            prev_tags = temp_y[i-n+1:i]
+            trans_p = self.trans_prob(tag,prev_tags)
+            prob *= trans_p
+
+        return prob
  
     def prob(self, x, y):
         """
@@ -49,6 +69,14 @@ class HMM:
         x -- sentence.
         y -- tagging.
         """
+
+        #en tag prob ya tenemos calculado q, falta calcular los productos de e
+        e = 1.0
+        tag_p = self.tag_prob(y)
+        for w,t in zip(x,y):
+            e *= self.out_prob(w,t)
+
+        return tag_p * e
  
     def tag_log_prob(self, y):
         """
@@ -56,7 +84,22 @@ class HMM:
  
         y -- tagging.
         """
- 
+        #Generar el producto de los q
+        prob = 0.0
+        temp_y = ['<s>']*(self.n-1)+ list(y) + ['</s>']
+        n = self.n
+        for i in range(n-1,len(temp_y)):
+            tag = temp_y[i]
+            prev_tags = temp_y[i-n+1:i]
+            trans_p = self.trans_prob(tag,prev_tags)
+            if trans_p == 0:
+                prob = float('-inf')
+                break
+
+            prob += log2(trans_p)
+
+        return prob
+
     def log_prob(self, x, y):
         """
         Joint log-probability of a sentence and its tagging.
@@ -64,6 +107,19 @@ class HMM:
         x -- sentence.
         y -- tagging.
         """
+
+        e = 0.0
+        tag_p = self.tag_log_prob(y)
+        prob_e = 0.0
+        for w,t in zip(x,y):
+            e = self.out_prob(w,t)
+            if e == 0:
+                prob_e = float('-inf')
+                break
+            prob_e += log2(e)
+
+
+        return tag_p + prob_e
  
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
@@ -85,7 +141,7 @@ class ViterbiTagger:
         self.trans = self.hmm.trans
         self.trans_prob = self.hmm.trans_prob
         self.out_prob = self.hmm.out_prob
-        self.tagset = self.hmm.tagset
+        self.tag_set = self.hmm.tag_set
         self.n = self.hmm.n
 
         pi[0][tuple(['<s>']*(self.hmm.n-1))] = (0,[])
@@ -99,7 +155,7 @@ class ViterbiTagger:
         m = len(sent)
         pi = self._pi
         for k in range(1,m+1):
-        	for tag in self.tagset:
+        	for tag in self.tag_set:
         		for prev_tags, (prob, tags) in pi[k-1].items():
         			q = self.hmm.trans_prob(tag,prev_tags)
         			e = self.hmm.out_prob(sent[k-1],tag)
@@ -115,7 +171,7 @@ class ViterbiTagger:
         resul_tag = []
         for prev_tags, (prob, tags) in pi[m].items():
         	q = self.hmm.trans_prob('</s>', prev_tags)
-        	if q > 0.0
+        	if q > 0.0:
         		prob += log2(q)
         		if max_prob < prob:
         			max_prob = prob
