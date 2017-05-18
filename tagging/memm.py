7047edffb309c481cc7de4ab3f4e43adaa67ef13
Robertino Nicolazzi <robertnn@outlook.com>
2017-05-18 11:25:48 -0300

    inicializando memm, falta generar historias para poder entrenar

--- tagging/memm.py
+++ tagging/memm.py
@@ -1,3 +1,9 @@
+from featureforge.vectorizer import Vectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline
+
+from tagging.features import History,word_lower,word_istitle,word_isupper,word_isdigit,NPrevTags,PrevWord
+
 class MEMM:
  
     def __init__(self, n, tagged_sents):
@@ -6,13 +12,46 @@ class MEMM:
         tagged_sents -- list of sentences, each one being a list of pairs.
         """
  
+ 		#Agregar features anteriores al vectorezer
+ 		self.vocabulary= set()
+ 		self.tag_set = set()
+
+ 		temp_features = [word_lower, word_istitle,word_isdigit,word_isupper]
+ 		features=[word_lower, word_istitle,word_isdigit,word_isupper]
+ 		#agregamos los features no basicos
+ 		for i in range(self.n):
+ 			features.append(NPrevTags(i))
+
+ 		for feature in temp_features:
+ 			features.append(PrevWord(feature))
+
+ 		#Obtener conjunto de palabras conocidas y de tags
+        for sent in tagged_sents:
+            tags = tuple(tag for (_,tag) in sent)
+            words = tuple(word for (word,_) in sent)
+            self.vocabulary = self.vocabulary.union(words)
+            self.tag_set = self.tag_set.union(tags)
+
+
+ 		self.text_clf = Pipeline([('vect', Vectorizer(features)),
+                     ('clf', MultinomialNB()),
+		])
+
+		#Para entrenar necesitamos las historias y los tags
+
+
     def sents_histories(self, tagged_sents):
         """
         Iterator over the histories of a corpus.
  
         tagged_sents -- the corpus (a list of sentences)
         """
- 
+        histories = []
+        for sent in tagged_sents:
+            histories += self.sent_histories(sent)
+
+		return histories
+		 		
     def sent_histories(self, tagged_sent):
         """
         Iterator over the histories of a tagged sentence.
