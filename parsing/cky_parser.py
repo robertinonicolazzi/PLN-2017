b60aaf7f66bc114ae1392d8f4f34b4f596da0e72
Robertino Nicolazzi <robertnn@outlook.com>
2017-05-31 18:17:28 -0300

    Hay diferentes tipos en los diccionarios, transformar claves de subdiccionarios en simbolos

---
+++ parsing/cky_parser.py
@@ -0,0 +1,66 @@
+from collections import defaultdict
+from nltk.tree import Tree
+
+class CKYParser:
+ 
+    def __init__(self, grammar):
+        """
+        grammar -- a binarised NLTK PCFG.
+        """
+
+        self.grammar = grammar
+
+        #Extraemos las probabilidades de q(X->YZ) y de q(X->xi)
+        self._probs_dict = _probs_dict = defaultdict(dict)
+        for p in grammar.productions():
+            lhs = p.lhs()
+            rhs = p.rhs()
+            _probs_dict[tuple(rhs)][lhs] = p.logprob()
+
+
+    def parse(self, sent):
+        """Parse a sequence of terminals.
+ 
+        sent -- the sequence of terminals.
+        """
+        n = len(sent)
+        self._pi = pi = defaultdict(dict)
+        self._bp = bp = defaultdict(dict)
+        probs_dict = self._probs_dict
+        #Inicializamos los q(X->xi)
+
+        for i, word in enumerate(sent, start=1):
+            #Buscamos los X tal que X->word
+
+            for X, log_prob in probs_dict[(word,)].items():
+                pi[(i,i)][X] = log_prob
+                bp[(i,i)][X] = Tree(X,[word])
+
+        for l in range(1,n):
+            for i in range(1,n+1-l):
+                j = l + i
+
+                #Agarramos los YZ tal que X->YZ para cada s E {i...(j-1)}
+                for s in range(i,j):
+                    #Generamos posibles YZ
+
+                    for Y in pi[(i,s)].keys():
+                        for Z in pi[(s+1,j)].keys():
+
+                            #Obtenemos los X
+                            for X, logprob in probs_dict.get((Y,Z),{}).items():
+                                pi_y = pi[(i,s)][Y]
+                                pi_z = pi[(s+1,j)][Z]
+                                log_prob = log_prob + pi_z + pi_y
+
+                                if log_prob > pi[(i,j)].get(X,float('-inf')):
+                                    pi[(i,j)][X] = log_prob
+                                    bp[(i,j)][X] = Tree(X,[bp[(i,s)][Y],bp[(s+1,j)][Z] ])
+
+        # Obtener el mejor arbol y la mejor probabilidad
+
+        result_prob = pi[(1,n)].get(self.grammar.start().symbol(), float('-inf'))
+        result_tree = bp[(1,n)].get(self.grammar.start().symbol(),None)
+
+        print (pi)
+        return (result_prob, result_tree)
